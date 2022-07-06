057bff84c31638b469c4e04d9c300965bb4a913a
John <joohnzay@gmail.com>
2022-07-06 16:14:31 -0300

    Primeiro site feito usando html, css e javascript do zero

---
+++ Projeto Final/go.js
@@ -0,0 +1,9 @@
+function img01() {
+    document.getElementById("trocarimg").src ='Imagenss/graphic-intro-widgets.png'
+  }
+  function img02() {
+    document.getElementById("trocarimg").src ='Imagenss/graphic-intro-code.png'
+  }
+  function img03() {
+    document.getElementById("trocarimg").src ='Imagenss/graphic-intro-plugins.png'
+  }
